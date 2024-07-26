e46b18c93f5b01fa5ab21fdfb279fe7d1d4c25a1
cassioHilario <cacaafh2000@gmail.com>
2024-07-26 15:15:53 -0300

    Stateless component

--- meuapp/src/App.js
+++ meuapp/src/App.js
@@ -1,8 +1,26 @@
-export default function APp(){
+import React from 'react';
+
+
+const HelloWord = () => {
+  return(
+    <h1>Hello World</h1>
+  )
+}
+
+
+const Welcome = (props) => {
+  return(
+    <h3>Welcome, {props.name}</h3>
+  )
+}
+
+
+export default function App(){
   return(
     <div>
-      <h1>Hello World</h1>
+      <HelloWord />
       <h2>Desenvolvendo meu primeiro App React.</h2>
+      <Welcome name="Cassio" />
     </div>
   )
 }

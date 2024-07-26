c540ab5ac4cba88299653fecd84768d6ec23dd75
cassioHilario <cacaafh2000@gmail.com>
2024-07-26 17:15:11 -0300

    Working with conditional rendering

--- meuapp/src/App.js
+++ meuapp/src/App.js
@@ -1,11 +1,34 @@
 import React, { Component } from 'react';
-import Member from './components/Member';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      status: true
+    };
+  }
+
   render() {
     return (
       <div>
-        <Member name="Cassio"></Member>
+        
+        {this.state.status 
+          ?
+
+          <div>
+            <h1>Hello Cassio!</h1>
+            <button onClick={() => this.setState({status: false})}>Logout</button>
+          </div>
+
+          :
+          
+          <div>
+            <h1>Hello World!</h1>
+            <button onClick={() => this.setState({status: true})}>Login</button>
+          </div>
+        }
+
       </div>
     );
   }

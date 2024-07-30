c498d2ff38441512ee31b3175ee43320db3d8002
Cassio Hilario <cassio.hilario@biomtech.com.br>
2024-07-30 14:27:11 -0300

    Working with hooks: useEffect

--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -1,13 +1,38 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 function App() {
 
-  const [tasks, setTasks] = useState([
-    'Task 1',
-    'Task 2',
-  ]);
+  const [tasks, setTasks] = useState([...returnStoredTaskList()]);
   
   const [newTask, setNewTask] = useState('');
+
+  // Behaves like componentDidMount
+  useEffect(() => {
+    const storedTasks = localStorage.getItem('tasks');
+    if (storedTasks) {
+      setTasks(JSON.parse(storedTasks));
+    }
+    return () => {};
+  }, []);
+
+  // Behaves like componentDidUpdate
+  useEffect(() => {
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+    return () => {};
+  }, [tasks]);
+
+  function handleAddTask(){
+    setTasks([...tasks, newTask]);
+    setNewTask('');
+  }
+
+  function returnStoredTaskList(){
+    const storedTasks = localStorage.getItem('tasks');
+    if (storedTasks) {
+      return JSON.parse(storedTasks);
+    }
+    return [];
+  }
 
   return (
     <div>
@@ -17,7 +42,7 @@ function App() {
         ))}
       </ul>
       <input type='text' value={newTask} onChange={(e) => setNewTask(e.target.value)}/>
-      <button type='button' onClick={() => setTasks([...tasks, newTask])}>Add task</button>
+      <button type='button' onClick={handleAddTask}>Add task</button>
     </div>
   );
 }
